e31cbc6c7d8bf9be83415596f0cf4a6680ff147f
aelanman <adam_lanman@brown.edu>
2016-06-29 10:36:40 -0400

    parallel uvfits_zeros fix

--- ael/parallel_version/make_uvfits.sh
+++ ael/parallel_version/make_uvfits.sh
@@ -10,4 +10,4 @@ mem='10G'
 time='1:00:00'
 
 
-sbatch -o /dev/null --array=0-$N --mem=$mem -t $time -n 3 --export=N=$N,fname=$fname,paramfile=$paramfile,calfile=$calfile zeros_job.sh
+sbatch -p jpober-test -o /dev/null --array=0-$N --mem=$mem -t $time -n 3 --export=N=$N,fname=$fname,paramfile=$paramfile,calfile=$calfile zeros_job.sh
--- ael/parallel_version/zeros_job.sh
+++ ael/parallel_version/zeros_job.sh
@@ -3,4 +3,4 @@
 #SBATCH -J uvfits_zeros
 i=$SLURM_ARRAY_TASK_ID
 
-python uvfits_zeros_par.py $calfile $paramfile -i $i -N $N -o $fname
+python uvfits_zeros_par.py $calfile $paramfile -i $i -N $N -o `pwd `'/'$fname
