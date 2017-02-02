b10e97df52390dde45d1f0e2a4427fcf4a0eee6e
Josh Kerrigan <Josh@Joshs-Air.home>
2017-02-01 19:58:23 -0500

    BGMM updated with functions

--- jrk/BGMM.py
+++ jrk/BGMM.py
@@ -17,61 +17,60 @@ from scipy import linalg
 import itertools
 
 o = optparse.OptionParser()
-sep='(0,1)'
+sep=('0,1','-1,1','1,1')
 opts,args = o.parse_args(sys.argv[1:])
 color_iter = itertools.cycle(['navy', 'c', 'cornflowerblue', 'gold',
                               'darkorange'])
 
-BLS=os.popen("python ./capo/pspec_pipeline/getbls.py --sep='0,1' -C psa6240_FHD /Users/Josh/Desktop/PSA64Noise/DATA/zen.2456242.30605.uvcRREcACOTUcA").read()
-BLS = BLS.split(',')
+#BLS=os.popen("python ./capo/pspec_pipeline/getbls.py --sep='0,1' -C psa6240_FHD /Users/Josh/Desktop/PSA64Noise/DATA/zen.2456242.30605.uvcRREcACOTUcA").read()
+#BLS = BLS.split(',')
 base = uvdata.miriad.Miriad()
-data = n.zeros((14,203,len(BLS)))
-for files in args:
-#    print 'Adding..',files
-    if files == args[0]:
-        try:
-            base.read_miriad(files)
-        except:
-            pass
-        for b in range(len(BLS)):
-#            print BLS[b]
-            b1,b2 = BLS[b].split('_')
-            bls = base.baseline_array==base.antnums_to_baseline(b1,b2)
-            if base.data_array[bls,0,:,0].shape[0] == 0:
-                continue
-            else:
-                data[:,:,b] = base.data_array[bls,0,:,0]
-    else:
-        print 'Error Will Robinson...Error'
-        del(mir)
-data = data[:,:,:]
-mus = n.mean(data,2)
-MAX = n.max(data,2)
-MED = n.median(data,2)
-MIN = n.min(data,2)
-SKU = skew(data,axis=2)
-KURT = kurtosis(data,axis=2)
-stds = n.var(data,2)
-phs = n.mean(n.angle(data),2)
-phsKurt = kurtosis(n.angle(data),2)
-idx1 = (n.abs(phsKurt)> 2.0).astype(int)
-idx2 = (n.nan_to_num(n.log10(stds))>3.7).astype(int)
-idx3 = (n.nan_to_num(n.log10(stds))>1.4).astype(int) 
-#IDX = ((idx1+idx2+idx3)/3.).astype(bool)
-IDX = ((idx1+idx1)/2).astype(bool)
+#data = n.zeros((14,203,len(BLS)))
+try:
+    base.read_miriad(args[0])
+except:
+    pass
 
-shps = data.shape
-print shps
-X = n.zeros((shps[0]*shps[1],7))
-X[:,0] = mus.reshape(shps[0]*shps[1])
-X[:,1] = MAX.reshape(shps[0]*shps[1])
-X[:,2] = phsKurt.reshape(shps[0]*shps[1])
-X[:,3] = MIN.reshape(shps[0]*shps[1])
-X[:,4] = SKU.reshape(shps[0]*shps[1])
-X[:,5] = phs.reshape(shps[0]*shps[1])
-X[:,6] = stds.reshape(shps[0]*shps[1])
+def StackData(BLS):
+    data = n.zeros((14,203,len(BLS)))
+    for b in range(len(BLS)):
+        b1,b2 = BLS[b].split('_')
+        bls = base.baseline_array==base.antnums_to_baseline(b1,b2)
+        if base.data_array[bls,0,:,0].shape[0] == 0:
+            continue
+        else:
+            data[:,:,b] = base.data_array[bls,0,:,0]
+    return data
 
-dpgmm = mixture.BayesianGaussianMixture(n_components=2,covariance_type='full').fit(X)
+def DefFeat(data):
+    mus = n.mean(data,2)
+    MAX = n.max(data,2)
+    MED = n.median(data,2)
+    MIN = n.min(data,2)
+    SKU = skew(data,axis=2)
+    KURT = kurtosis(data,axis=2)
+    stds = n.var(data,2)
+    phs = n.mean(n.angle(data),2)
+    phsKurt = kurtosis(n.angle(data),2)
+    shps = data.shape
+    X = n.zeros((shps[0]*shps[1],7))
+    X[:,0] = mus.reshape(shps[0]*shps[1])
+    X[:,1] = MAX.reshape(shps[0]*shps[1])
+    X[:,2] = phsKurt.reshape(shps[0]*shps[1])
+    X[:,3] = MIN.reshape(shps[0]*shps[1])
+    X[:,4] = SKU.reshape(shps[0]*shps[1])
+    X[:,5] = phs.reshape(shps[0]*shps[1])
+    X[:,6] = stds.reshape(shps[0]*shps[1])
+    return X
+
+def GMMask(X):
+    dpgmm = mixture.BayesianGaussianMixture(n_components=2,covariance_type='full').fit(X)
+    Y = dpgmm.predict(X)
+    _Y_ = Y.reshape(14,203)
+    MASK = n.zeros((14,203))
+    MASK[_Y_==1] = 1.
+    return MASK
+
 
 def plot_results(X, Y_, means, covariances, index, title):
     splot = pl.subplot(1, 1, 1 + index)
@@ -85,52 +84,25 @@ def plot_results(X, Y_, means, covariances, index, title):
     pl.yticks(())
     pl.title(title)
 
-plot_results(X, dpgmm.predict(X), dpgmm.means_, dpgmm.covariances_, 0,'BGMM')
-pl.show()
-
-Y = dpgmm.predict(X)
-print Y.shape
-print Y.reshape(14,203)
-_Y_ = Y.reshape(14,203)
-
-
-MASK = n.zeros((14,203))
-MASK[_Y_==1] = 1.
-
-#MASK[_Y_==1] = 1.
-#pl.figure()
-#pl.subplot(311)
-#pl.imshow(n.log10(n.abs(data[:,:,10])),aspect='auto',interpolation=None)
-#pl.subplot(312)
-#pl.imshow(MASK,aspect='auto',interpolation=None)
-#pl.subplot(313)
-#pl.imshow(n.log10(n.abs(data[:,:,10]*n.logical_not(MASK))),aspect='auto',interpolation=None)
+#plot_results(X, dpgmm.predict(X), dpgmm.means_, dpgmm.covariances_, 0,'BGMM')
 #pl.show()
-#print IDX.sum()
-#pl.subplot(211)
-#pl.imshow(n.log10(n.abs(data[:,10])),aspect='auto',interpolation=None)
-#pl.subplot(212)
-#pl.imshow(IDX.astype(float),aspect='auto',interpolation=None)
-#pl.show()
-for b in range(len(BLS)):
-    b1,b2 = BLS[b].split('_')
-    bls = base.baseline_array==base.antnums_to_baseline(b1,b2)
-    if base.flag_array[bls,0,:,0].shape[0] == 0:
-        continue
-    else:
-        print base.flag_array[bls,0,:,0][_Y_==1].shape
-        base.flag_array[bls,0,:,0] += MASK.astype(bool)
+
+def AddFlags(MASK,BLS):
+    for b in range(len(BLS)):
+        b1,b2 = BLS[b].split('_')
+        bls = base.baseline_array==base.antnums_to_baseline(b1,b2)
+        if base.flag_array[bls,0,:,0].shape[0] == 0:
+            continue
+        else:
+            base.flag_array[bls,0,:,0] += MASK.astype(bool)
+
+for s in sep:
+    print s
+    BLS=os.popen("python /Users/Josh/Desktop/PSA64Noise/capo/pspec_pipeline/getbls.py --sep="+s+" -C psa6240_FHD /Users/Josh/Desktop/PSA64Noise/DATA/zen.2456242.30605.uvcRREcACOTUcA").read()
+    print BLS
+    BLS = BLS.split(',')
+    D = StackData(BLS)
+    X = DefFeat(D)
+    M = GMMask(X)
+    AddFlags(M,BLS)
 base.write_miriad(args[0]+'r')
-#pl.figure()
-#pl.subplot(411)
-#pl.scatter(mus,phs)
-#pl.subplot(412)
-#pl.scatter(n.abs(MAX),n.abs(MIN))
-#pl.subplot(413)
-#pl.scatter(n.abs(SKU),n.abs(mus))
-#pl.subplot(414)
-#pl.scatter(n.abs(SKU),n.abs(KURT))
-#pl.show()
-#pl.scatter(mus[IDX],n.log10(stds[IDX]),color='r')
-#pl.scatter((mus),n.log10(stds))
-
