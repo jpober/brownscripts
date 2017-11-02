9af2f33fcceb2c6ca3b16ffc9c4ff4ce2c59665f
wenyang-li <wenyang_li@brown.edu>
2017-11-02 14:07:37 -0400

    mvis2uvd bug fix

--- wyl/mvis2uvd.py
+++ wyl/mvis2uvd.py
@@ -18,50 +18,97 @@ uv.read_fhd(fhdlist,run_check=False,run_check_acceptability=False)
 print '     Loading mdlvis'
 npz_x = np.load(opts.solpath+obsid+'.xx.omni.npz')
 npz_y = np.load(opts.solpath+obsid+'.yy.omni.npz')
+ant = []
+for k in npz_x.keys():
+    if k[0].isdigit(): ant.append(int(k[0:-1]))
+ant.sort()
 mdvis = {'xx':{}, 'yy':{}}
-reds = mp2cal.wyl.cal_reds_from_pos(antpos)
+info = mp2cal.wyl.pos_to_info(antpos,ants=ant)
+a1, a2 = [], []
+for ii in range(info.nAntenna):
+    for jj in range(ii,info.nAntenna):
+        a1.append(ii)
+        a2.append(jj)
+ant_dict = {}
+for a in ant: ant_dict[info.ant_index(a)] = a
+reds = info.get_reds()
 reds_ind = {}
 ubls = []
 chix = npz_x['chisq2']
 chiy = npz_y['chisq2']
-maskx = np.zeros(chix.shape)
-masky = np.zeros(chiy.shape)
-indx = np.where(chix < 1.2)
-indy = np.where(chiy < 1.2)
-maskx[indx] = 1
-masky[indy] = 1
+maskx = chix > 1.2
+masky = chiy > 1.2
+flag = {}
+flag['xx'] = np.logical_or(npz_x['flags'], maskx)
+flag['yy'] = np.logical_or(npz_y['flags'], masky)
 for key in npz_x.keys():
     if key.startswith('<'):
         bl,pol = key.split()
         bl = tuple(map(int,bl[1:-1].split(',')))
-        mdvis[pol][bl] = npz_x[key]*maskx
+        mdvis[pol][bl] = npz_x[key]
         ubls.append(bl)
 for key in npz_y.keys():
     if key.startswith('<'):
         bl,pol = key.split()
         bl = tuple(map(int,bl[1:-1].split(',')))
-        mdvis[pol][bl] = npz_y[key]*masky
-uv.data_array *= 0
+        mdvis[pol][bl] = npz_y[key]
 for r in reds:
     ubl = None
     for bl in ubls:
         if bl in r: ubl = bl
     if ubl is None: continue
     for b in r: reds_ind[b] = ubl
-a1 = uv.ant_1_array[:uv.Nbls]
-a2 = uv.ant_2_array[:uv.Nbls]
+Nbls0 = uv.Nbls
+Nbls1 = info.nAntenna*(info.nAntenna+1)/2
+b0 = 128*uv.ant_1_array[:Nbls0] + uv.ant_2_array[:Nbls0]
+uv.Nbls = Nbls1
+uv.Nants_data = info.nAntenna
+uv.Nants_telescope = info.nAntenna
+uv.Nblts = uv.Ntimes*uv.Nbls
+times = np.resize(np.unique(uv.time_array),(uv.Nbls,uv.Ntimes)).T
+uv.time_array = np.resize(times,(times.size))
+lsts = np.resize(np.unique(uv.lst_array),(uv.Nbls,uv.Ntimes)).T
+uv.lst_array = np.resize(lsts,(lsts.size))
+uvw = np.zeros((uv.Nblts,3))
+nsample = np.zeros((uv.Nblts,uv.Nspws,uv.Nfreqs,uv.Npols))
+uv.ant_1_array = np.array(a1*uv.Ntimes)
+uv.ant_2_array = np.array(a2*uv.Ntimes)
+b1 = 128*uv.ant_1_array[:Nbls1] + uv.ant_2_array[:Nbls1]
+for ii in range(uv.Nbls):
+    i = b1[ii]/128
+    j = b1[ii]%128
+    ai = ant_dict[i]
+    aj = ant_dict[j]
+    try:
+        ind = np.where(b0 == 128*ai + aj)[0][0]
+        uvw[ii::Nbls1] = uv.uvw_array[ind::Nbls0]
+    except:
+        ind = np.where(b0 == 128*aj + ai)[0][0]
+        uvw[ii::Nbls1] = -uv.uvw_array[ind::Nbls0]
+    nsample[ii::Nbls1] = uv.nsample_array[ind::Nbls0]
+uv.uvw_array = uvw
+uv.nsample_array = nsample
+uv.data_array = np.zeros((uv.Nblts,uv.Nspws,uv.Nfreqs,uv.Npols),dtype=np.complex64)
+uv.flag_array = np.zeros((uv.Nblts,uv.Nspws,uv.Nfreqs,uv.Npols),dtype=bool)
+uv.baseline_array = uv.antnums_to_baseline(uv.ant_1_array,uv.ant_2_array)
+uv.antenna_positions = np.zeros((info.nAntenna,3))
+uv.antenna_numbers = np.arange(info.nAntenna)
+uv.antenna_names = []
+for ii in range(info.nAntenna): uv.antenna_names.append(str(ii))
+
 for pp in ['xx','yy']:
     pn = aipy.miriad.str2pol[pp]
     pid = np.where(uv.polarization_array==pn)[0][0]
     for ii in range(uv.Nbls):
         i = a1[ii]
         j = a2[ii]
-        if (i,j) in reds_ind.keys(): vis = mdvis[pp][reds_ind[(i,j)]]
-        elif (j,i) in reds_ind.keys(): vis = mdvis[pp][reds_ind[(j,i)]].conj()
+        ai = ant_dict[i]
+        aj = ant_dict[j]
+        if (ai,aj) in reds_ind.keys(): vis = mdvis[pp][reds_ind[(ai,aj)]]
+        elif (aj,ai) in reds_ind.keys(): vis = mdvis[pp][reds_ind[(aj,ai)]].conj()
         else: continue
         uv.data_array[:,0][:,:,pid][ii::uv.Nbls] = vis
-ind = np.where(uv.data_array == 0)
-uv.flag_array[ind] = True
+        uv.flag_array[:,0][:,:,pid][ii::uv.Nbls] = flag[pp]
 outuvfits = opts.outpath + obsid + '_mvis.uvfits'
 print '     Writing ' + outuvfits
 uv.write_uvfits(outuvfits,spoof_nonessential=True)
