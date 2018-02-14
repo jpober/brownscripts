Fixed phase sign error

jtb/bayes/nudft.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.gridspec as gridspec
 import optparse, sys, os
 
-from scipy.integrate import dblquad
 from matplotlib.pyplot import *
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
@@ -15,6 +14,19 @@ o.add_option('--pixel_count',
     type = int,
     help = 'Number of pixels along one axis in sky image.  Total number of pixels is pixel_count**2.',
     default = 50)
+o.add_option('--cmap',
+    type = str,
+    help = 'Colormap to use for plotting.',
+    default = 'gnuplot')
+o.add_option('--plot_phase',
+    action = 'store_true',
+    help = 'If passed, also plot and compare phase.')
+o.add_option('--l_offset',
+    type = float,
+    help = 'Moves source in l-direction by l_offset*ls.max().  Must be between 0 and 1.')
+o.add_option('--m_offset',
+    type = float,
+    help = 'Moves source in m-direction by m_offset*ms.max().  Must be between 0 and 1.')
 opts,args = o.parse_args(sys.argv[1:])
 
 ## ----------------- Construct sky ----------------- ##
@@ -33,7 +45,17 @@ if len(ms) % 2 == 0:
     mid_m = len(ms)/2
 else:
     mid_m = int(len(ms)/2. - 0.5)
-I[mid_l, mid_m] = 1.0
+
+if opts.l_offset:
+    l_off = int(mid_l*opts.l_offset)
+else:
+    l_off = 0
+if opts.m_offset:
+    m_off = int(mid_m*opts.m_offset)
+else:
+    m_off = 0
+
+I[mid_m + m_off, mid_l + l_off] = 1.0
 N_freq = 1
 pixel_area = (ls[1]-ls[0])*(ms[1]-ms[0])
 
@@ -84,7 +106,7 @@ for u,v in uvs_unique:
 ## ----------------- Analytic solution comparison ----------------- ##
 
 # Point source, Flat beam
-Vs_func = lambda u,v: pixel_area*np.exp(2*np.pi*1j*(u*ls[mid_l] + v*ms[mid_m]))
+Vs_func = lambda u,v: pixel_area*np.exp(-2*np.pi*1j*(u*ls[mid_l + l_off] + v*ms[mid_m + m_off]))
 Vs_analytic = Vs_func(uvs_unique[:,0],uvs_unique[:,1])
 
 
@@ -92,58 +114,94 @@ Vs_analytic = Vs_func(uvs_unique[:,0],uvs_unique[:,1])
 
 ## ----------------- Plotting ----------------- ##
 
-fig = figure(figsize=(16,4))
-gs = gridspec.GridSpec(1,4)
+if opts.plot_phase:
+    fig = figure(figsize = (16,8))
+    gs = gridspec.GridSpec(2,4)
 
-imgs = []
-aspect = 'auto'
-cmap = 'gnuplot2'
+    imax = subplot(gs[:,0])
+    visax = subplot(gs[0,1])
+    vispax = subplot(gs[1,1])
+    anax = subplot(gs[0,2])
+    anpax = subplot(gs[1,2])
+    diffax = subplot(gs[0,3])
+    diffpax = subplot(gs[1,3])
+else:
+    fig = figure(figsize = (16,4))
+    gs = gridspec.GridSpec(1,4)
 
-ax = subplot(gs[0])
-ogim = ax.imshow(I,
-                            interpolation = 'nearest',
-                            origin = 'center',
-                            extent = extent_lm,
-                            aspect = aspect,
-                            cmap = cmap)
-ax.set_title('sky')
-ax.set_xlabel('l', size=16)
-ax.set_ylabel('m', size=16, rotation=0)
+    imax = subplot(gs[0])
+    visax = subplot(gs[1])
+    anax = subplot(gs[2])
+    diffax = subplot(gs[3])
 
-visax = subplot(gs[1])
+
+aspect = 'equal'
+
+ogim = imax.imshow(I,
+                                interpolation = 'nearest',
+                                origin = 'lower',
+                                extent = extent_lm,
+                                aspect = aspect,
+                                cmap = opts.cmap)
+imax.set_title('sky')
+imax.set_xlabel('l', size=16)
+imax.set_ylabel('m', size=16, rotation=0)
+
+
 myim = visax.scatter(uvs_unique[:,0],
                                 uvs_unique[:,1],
                                 c = np.abs(Vs_unique),
-                                cmap = cmap)
+                                cmap = opts.cmap)
 visax.set_title('my fft')
 
-anax = subplot(gs[2])
+
 anim = anax.scatter(uvs_unique[:,0],
                               uvs_unique[:,1],
                               c = np.abs(Vs_analytic),
-                              cmap = cmap)
+                              cmap = opts.cmap)
 anax.set_title('analytic solution')
 
-diffax = subplot(gs[3])
+
 diffim = diffax.scatter(uvs_unique[:,0],
                                 uvs_unique[:,1],
                                 c = np.abs(Vs_unique) - np.abs(Vs_analytic),
-                                cmap = cmap)
+                                cmap = opts.cmap)
 diffax.set_title('my fft - analytic solution')
 
-for a in fig.axes[1:]:
-    a.set_xlabel('u', size=16)
-    a.set_ylabel('v', size=16, rotation=0)
-    a.set_aspect('equal')
+if opts.plot_phase:
+    mypim = vispax.scatter(uvs_unique[:,0],
+                                       uvs_unique[:,1],
+                                       c = np.imag(Vs_unique),
+                                       cmap = opts.cmap)
 
-imgs = [ogim, myim, anim, diffim]
+    anpim = anpax.scatter(uvs_unique[:,0],
+                                     uvs_unique[:,1],
+                                     c = np.imag(Vs_analytic),
+                                     cmap = opts.cmap)
+
+    diffpim = diffpax.scatter(uvs_unique[:,0],
+                                        uvs_unique[:,1],
+                                        c = np.imag(Vs_unique) - np.imag(Vs_analytic),
+                                        cmap = opts.cmap)
+
+    imgs = [ogim, myim, mypim, anim, anpim, diffim, diffpim]
+
+else:
+    imgs = [ogim, myim, anim, diffim]
+
+
+for a in fig.axes[1:]:
+    a.set_xlabel('u', size = 16)
+    a.set_ylabel('v', size = 16, rotation = 0)
+    a.set_aspect('equal')
 
 for i,axe in enumerate(fig.axes):
     divider = make_axes_locatable(axe)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    colorbar(imgs[i], cax=cax)
-
+    cax = divider.append_axes("right", size = "5%", pad = 0.05)
+    colorbar(imgs[i], cax = cax)
 
 gs.tight_layout(fig)
 
 show()
+
+sys.exit()