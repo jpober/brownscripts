a313c25984f84dd09ca76e0ce3c2687abfd8d2a4
Jacob Burba <jacob_burba@brown.edu>
2018-03-01 16:56:48 -0500

    Compute uv sampling for multiple frequencies

--- jtb/bayes/uv_from_blns.py
+++ jtb/bayes/uv_from_blns.py
@@ -16,8 +16,11 @@ o.add_option('--positions',
     type=str,
     help='Filename with antenna positions.')
 o.add_option('--freq',
-    type=float,
-    help='Frequency of observation in MHz.')
+    type=str,
+    help='Frequency(ies) of observation in MHz.')
+o.add_option('--freq_res',
+    type = float,
+    help = 'Channel width in MHz if --freq passed with \'-\'.')
 o.add_option('--width',
     type=float,
     help='Width in degrees of observational period.')
@@ -38,6 +41,16 @@ if opts.data is None and opts.positions is None:
 # Setup
 c = 3.e8
 
+# Get frequencies to generate uv sampling for
+if ',' in opts.freq:
+    freqs = map(float, opts.freq.split(','))
+elif '-' in opts.freq:
+    freqs = map(float, opts.freq.split('-'))
+    # freqs = np.linspace(freqs[0], freqs[1], int((freqs[1] - freqs[0])/opts.freq_res) + 1)
+    freqs = np.arange(freqs[0], freqs[1] + opts.freq_res, opts.freq_res)
+elif not (',' in opts.freq and '-' in opts.freq):
+    freqs = [float(opts.freq)]
+
 if opts.snapshot:
     hours = np.array([np.pi/2])
 else:
@@ -45,6 +58,7 @@ else:
     high_lim = np.pi/2+0.5*opts.width*np.pi/180
     hours = np.linspace(low_lim, high_lim, 10)
 decs = np.ones_like(hours)*np.pi/2 #zenith transitting point source
+
 uv = UVData()
 if opts.data is not None:
     # Read in telescope data
@@ -73,46 +87,51 @@ elif opts.positions is not None:
                 and uv.antnums_to_baseline(ant2, ant1) not in blns):
                 blns = np.append(blns, uv.antnums_to_baseline(ant1, ant2))
 
-# Get baseline
-l = c/(opts.freq*1e6) #m, wavelength
-uvws = np.zeros((hours.shape[0], 3, blns.shape[0]))
-for i, bln in enumerate(blns):
-    antpair = uv.baseline_to_antnums(bln)
-    bln_vec = (ant_pos[antpair[1]-1,:] - ant_pos[antpair[0]-1,:])/l
-    uvws[:,0,i] = np.sin(hours)*bln_vec[0] + np.cos(hours)*bln_vec[1]
-    uvws[:,1,i] = -np.sin(decs)*(np.cos(hours)*bln_vec[0] - np.sin(hours)*bln_vec[1]) + np.cos(decs)*bln_vec[2]
-    uvws[:,2,i] = np.cos(decs)*(np.cos(hours)*bln_vec[0] - np.sin(hours)*bln_vec[2]) + np.sin(decs)*bln_vec[2]
+# New line, better output formatting
+print ''
+for freq in freqs:
+    # Get baseline
+    l = c/(freq*1e6) #m, wavelength
+    uvws = np.zeros((hours.shape[0], 3, blns.shape[0]))
+    for i, bln in enumerate(blns):
+        antpair = uv.baseline_to_antnums(bln)
+        bln_vec = (ant_pos[antpair[1]-1,:] - ant_pos[antpair[0]-1,:])/l
+        uvws[:,0,i] = np.sin(hours)*bln_vec[0] + np.cos(hours)*bln_vec[1]
+        uvws[:,1,i] = -np.sin(decs)*(np.cos(hours)*bln_vec[0] - np.sin(hours)*bln_vec[1]) + np.cos(decs)*bln_vec[2]
+        uvws[:,2,i] = np.cos(decs)*(np.cos(hours)*bln_vec[0] - np.sin(hours)*bln_vec[2]) + np.sin(decs)*bln_vec[2]
 
-if opts.write:
-    np.save('uvws.npy', uvws)
+    if opts.write:
+        filename = 'uvws_%sMHz.npy' %freq
+        print 'Writing ' + filename + ' ...\n'
+        np.save(filename, uvws)
 
-if opts.plot:
-    # Plotting
-    fig = figure(figsize=(10,4.5))
-    gs = gridspec.GridSpec(1,2)
+    if opts.plot:
+        # Plotting
+        fig = figure(figsize=(10,4.5))
+        gs = gridspec.GridSpec(1,2)
 
-    # Plot baseline(s) in position space
-    ax1 = subplot(gs[0], aspect='equal')
-    ax1.plot(ant_pos[:,0],ant_pos[:,1],'o')
-    ax1.set_xlabel('x [m]', size=18)
-    ax1.set_ylabel('y [m]', size=18)
+        # Plot baseline(s) in position space
+        ax1 = subplot(gs[0], aspect='equal')
+        ax1.plot(ant_pos[:,0],ant_pos[:,1],'o')
+        ax1.set_xlabel('x [m]', size=18)
+        ax1.set_ylabel('y [m]', size=18)
 
-    ax2 = subplot(gs[1], aspect='equal')
-    for i in range(blns.shape[0]):
-        ax2.plot(uvws[:,0,i], uvws[:,1,i], 'b.')
-        ax2.plot(-uvws[:,0,i], -uvws[:,1,i], 'b.')
-    ax2.set_xlabel('u', size=18)
-    ax2.set_ylabel('v', size=18)
+        ax2 = subplot(gs[1], aspect='equal')
+        for i in range(blns.shape[0]):
+            ax2.plot(uvws[:,0,i], uvws[:,1,i], 'b.')
+            ax2.plot(-uvws[:,0,i], -uvws[:,1,i], 'b.')
+        ax2.set_xlabel('u', size=18)
+        ax2.set_ylabel('v', size=18)
 
-    for ax in fig.axes:
-        ax.tick_params(axis='both',labelsize=16)
+        for ax in fig.axes:
+            ax.tick_params(axis='both',labelsize=16)
 
-    if opts.data:
-        suptitle(opts.data + ', ' + str(opts.freq) + ' MHz', size=16)
-    else:
-        suptitle(opts.positions + ', ' + str(opts.freq) + ' MHz', size=16)
+        if opts.data:
+            suptitle(opts.data + ', ' + str(freq) + ' MHz', size=16)
+        else:
+            suptitle(opts.positions + ', ' + str(freq) + ' MHz', size=16)
 
-    gs.tight_layout(fig, rect=[None, None, None, 0.95])
-    # tight_layout()
+        gs.tight_layout(fig, rect=[None, None, None, 0.95])
+        # tight_layout()
 
-    show()
+        show()
