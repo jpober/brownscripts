a9895d59c0a9101e2e806995ec33db28a8e436d2
Jacob Burba <jacob_burba@brown.edu>
2018-06-28 18:11:15 -0400

    Added healpy interpolation; fixed plotting

--- jtb/bayes/plot_beam_freq_variation.py
+++ jtb/bayes/plot_beam_freq_variation.py
@@ -59,35 +59,37 @@ beam_freqs = fits.getdata(opts.beam, extname='FREQS')
 
 nside = hp.npix2nside(beam_E.shape[0])
 thetas, phis = hp.pix2ang(nside, np.arange(beam_E.shape[0]))
-plot_phi_inds = np.where(np.logical_and(phis <= np.deg2rad(1), thetas <= np.pi/2.))[0]
+mean_diff = np.mean(np.diff(thetas)[np.diff(thetas) != 0.0])
+fit_thetas = np.arange(0, np.pi + mean_diff, mean_diff)
+fit_phis = np.zeros_like(fit_thetas)
 
 start_ind = np.where(beam_freqs == freqs[0])[0][0]
-# norm = Normalize(vmin = np.rad2deg(np.min(thetas[plot_phi_inds])),
-#                            vmax = np.rad2deg(np.max(thetas[plot_phi_inds])))
+# Normalize beam and get interpolated value along phi=0
+beam_E /= beam_E[:, start_ind:start_ind + nfreqs + 1].max()
+fit_beam = np.zeros((fit_thetas.size, freqs.size))
+for i in range(freqs.size):
+    fit_beam[:, i] = hp.get_interp_val(beam_E[:, start_ind + i], fit_thetas, fit_phis)
+
+# Color scale stuff
 norm = Normalize()
-norm.autoscale(np.rad2deg(thetas[plot_phi_inds]))
+norm.autoscale(np.rad2deg(fit_thetas))
 s_m = cm.ScalarMappable(cmap=cm.jet_r)
 s_m.set_array([])
 colors = s_m.to_rgba(np.linspace(0, 1, nfreqs))
-colors_phis = s_m.to_rgba(np.linspace(0, 1, plot_phi_inds.shape[0]))
-# colors = cm.jet_r(np.linspace(0, 1, nfreqs))
-# colors_phis = cm.jet_r(np.linspace(0, 1, plot_phi_inds.shape[0]))
+colors_phis = s_m.to_rgba(np.linspace(0, 1, fit_thetas.size))
 
-
-# thetas_inds, freq_inds = np.meshgrid(plot_phi_inds, np.arange(start_ind, start_ind+nfreqs+1))
-freq_inds, thetas_inds = np.meshgrid(np.arange(start_ind, start_ind+nfreqs), plot_phi_inds)
-grid_data = beam_E[thetas_inds, freq_inds]
-
+# Plotting stuff
 plot_freqs = beam_freqs[start_ind : start_ind + nfreqs+1]
-extent = [plot_freqs.min(), plot_freqs.max(),
-               np.rad2deg(thetas[plot_phi_inds]).min(),
-               np.rad2deg(thetas[plot_phi_inds]).max()]
+extent = [plot_freqs.min(),
+               plot_freqs.max(),
+               np.rad2deg(fit_thetas).min(),
+               np.rad2deg(fit_thetas).max()]
 
-
-for i in range(plot_phi_inds.shape[0]):
+fig = figure(figsize=(11,7))
+for i in range(fit_thetas.size):
     if opts.poly_order:
         fit_xs = np.copy(beam_freqs[start_ind:start_ind + nfreqs])
-        fit_ys = np.copy(beam_E[plot_phi_inds[i], start_ind:start_ind + nfreqs])
+        fit_ys = np.copy(fit_beam[i])
         coeffs = np.polyfit(fit_xs, fit_ys, opts.poly_order)
         quad_fit = np.zeros_like(fit_xs)
         for j in range(opts.poly_order + 1):
@@ -95,26 +97,23 @@ for i in range(plot_phi_inds.shape[0]):
 
         residuals = fit_ys - quad_fit
         plot(fit_xs, residuals,
-               label='%.1f deg' %np.rad2deg(thetas[plot_phi_inds[i]]),
+               label='%.1f deg' %np.rad2deg(fit_thetas[i]),
                color=colors_phis[i])
     else:
+        # plot(beam_freqs[start_ind:start_ind + nfreqs],
+        #        beam_E[plot_phi_inds[i], start_ind:start_ind + nfreqs],
+        #        label='%.1f deg' %np.rad2deg(thetas[plot_phi_inds[i]]),
+        #        color=colors_phis[i])
         plot(beam_freqs[start_ind:start_ind + nfreqs],
-               10*np.log10(beam_E[plot_phi_inds[i], start_ind:start_ind + nfreqs]),
-               label='%.1f deg' %np.rad2deg(thetas[plot_phi_inds[i]]),
+               10*np.log10(fit_beam[i]),
+               label='%.1f deg' %np.rad2deg(fit_thetas[i]),
                color=colors_phis[i])
+        ylabel('dB', size=16)
 xlabel('Frequency [MHz]', size=16)
-ylabel('dB', size=16)
 tick_params(which='x', labelsize=16)
-# colorbar(s_m)
-# # Shrink current axis by 20%
-# ax = gca()
-# box = ax.get_position()
-# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-#
-# # Put a legend to the right of the current axis
-# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=2)
+cb = colorbar(s_m)
+cb.set_label(r'Polar Angle / $\pi$', size=16)
 
-# legend(loc='best', ncol=2)
 tight_layout()
 show()
 
@@ -125,6 +124,10 @@ show()
 
 
 ## ----------------- Extra Code ----------------- ##
+# thetas_inds, freq_inds = np.meshgrid(plot_phi_inds, np.arange(start_ind, start_ind+nfreqs+1))
+# freq_inds, thetas_inds = np.meshgrid(np.arange(start_ind, start_ind+nfreqs), plot_phi_inds)
+# grid_data = beam_E[thetas_inds, freq_inds]
+
 # fig = figure(figsize=(8,3))
 # fig = figure()
 # im = imshow(10*np.log10(grid_data), extent=extent, origin='upper', aspect='auto')
@@ -136,6 +139,16 @@ show()
 # cb = fig.colorbar(im, cax=cax)
 # cb.set_label('dB', size=16)
 
+# colorbar(s_m)
+# # Shrink current axis by 20%
+# ax = gca()
+# box = ax.get_position()
+# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+#
+# # Put a legend to the right of the current axis
+# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=2)
+
+# legend(loc='best', ncol=2)
 
 # for i in range(start_ind, start_ind + nfreqs):
 #     plot(np.rad2deg(thetas[plot_phi_inds]),
